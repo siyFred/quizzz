70321b8e72aa7e1e728acb3ec374f04c5f423632
Friederich <siyfred1n@gmail.com>
2025-04-14 00:20:16 -0300

    corrections

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
 
-import 'package:quizzz/home/widgets/app_bar_widget.dart';
+import 'package:quizzz/home/widgets/appbar/app_bar_widget.dart';
 
 class HomePage extends StatefulWidget {
-  HomePage({Key? key}) : supper(key: key);
+  HomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  const HomePage({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
